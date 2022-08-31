9614ddc94ad2f3e1f45fe0fb46714aa85d175ce4
Hasan-Mac <hasan@klapify.com>
2022-08-31 23:56:33 +0600

    init app

---
+++ app/src/main/java/com/mahmudul/localappupdate/HomeActivity.kt
@@ -0,0 +1,92 @@
+package com.mahmudul.localappupdate
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.mahmudul.easyappupdate.DownloadApp
+
+class HomeActivity : AppCompatActivity() {
+    private var versionText: TextView? = null
+    private var version: String? = null
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        try {
+            val pInfo = this.packageManager.getPackageInfo(packageName, 0)
+            version = pInfo.versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+        versionText = findViewById(R.id.txt_version)
+        with(versionText) { this?.setText("Current Version $version") }
+    }
+    /** TODO: Must need to check the External Storage Permission Because we are storing the
+     * ApK in the External Or Internal Storage.
+     */
+    private fun checkWriteExternalStoragePermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this@HomeActivity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            downloadTask()
+        } else {
+            requestWriteExternalStoragePermission()
+        }
+    }
+
+    private fun requestWriteExternalStoragePermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this@HomeActivity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this@HomeActivity,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                MY_PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE
+            )
+        } else {
+            ActivityCompat.requestPermissions(
+                this@HomeActivity,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                MY_PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == MY_PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE && grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            downloadTask()
+        } else {
+            Toast.makeText(this@HomeActivity, "Permission Not Granted.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun download(view: View?) {
+        checkWriteExternalStoragePermission()
+    }
+
+    private fun downloadTask() {
+        val downloadApk = DownloadApp(this@HomeActivity)
+        downloadApk.startDownloadingApk(
+            "",
+            "Update 2.0"
+        )
+    }
+
+    companion object {
+        private const val MY_PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE = 1001
+    }
+}
---
+++ easyappupdate/src/main/java/com/mahmudul/easyappupdate/DownloadApp.kt
@@ -0,0 +1,151 @@
+package com.mahmudul.easyappupdate
+
+import android.app.ProgressDialog
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.AsyncTask
+import android.os.Build
+import android.os.Environment
+import android.util.Log
+import android.webkit.URLUtil
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+
+open class DownloadApp (var context: Context) : AppCompatActivity() {
+
+    @JvmOverloads
+    fun startDownloadingApk(url: String, fileName: String = "App Update") {
+        if (URLUtil.isValidUrl(url)) {
+            DownloadNewVersion(context, url, fileName).execute()
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    private class DownloadNewVersion(
+        val context: Context,
+        val downloadUrl: String,
+        val fileName: String
+    ): AsyncTask<String, Int, Boolean>() {
+        private lateinit var bar: ProgressDialog
+        override fun onPreExecute() {
+            super.onPreExecute()
+            bar = ProgressDialog(context).apply {
+                setCancelable(false)
+                setMessage("Downloading...")
+                isIndeterminate = true
+                setCanceledOnTouchOutside(false)
+                show()
+            }
+        }
+
+        @Deprecated("Deprecated in Java")
+        override fun onProgressUpdate(vararg values: Int?) {
+            super.onProgressUpdate(*values)
+            var msg = ""
+            val progress = values[0]
+            if (progress != null) {
+                bar.progress = progress
+                msg = if (progress > 99) "Finishing... " else "Downloading... $progress%"
+            }
+
+            bar.apply {
+                isIndeterminate  = false
+                max = 100
+                setMessage(msg)
+            }
+        }
+
+        @Deprecated("Deprecated in Java")
+        override fun onPostExecute(result: Boolean?) {
+            super.onPostExecute(result)
+            bar.dismiss()
+            if (result != null && result) {
+                Toast.makeText(context, "Update Done", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, "Error: Try Again", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        @Deprecated("Deprecated in Java")
+        override fun doInBackground(vararg p0: String?): Boolean {
+            var flag = false
+
+            try {
+                val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString() + "/"
+                var outputFile = File("$path$fileName.apk")
+                var repetition = 1
+                while (outputFile.exists()) {
+                    outputFile = File("$path$fileName ($repetition).apk")
+                    repetition++
+                }
+
+                val directory = File(path)
+                if (!directory.exists()) {
+                    directory.mkdirs()
+                }
+
+                val url = URL(downloadUrl)
+                val c = url.openConnection() as HttpURLConnection
+                c.requestMethod = "GET"
+                c.connect()
+
+                val fos = FileOutputStream(outputFile)
+                val inputStream = c.inputStream
+                val totalSize = c.contentLength.toFloat() //size of apk
+
+                val buffer = ByteArray(1024)
+                var len1: Int
+                var per: Float
+                var downloaded = 0f
+                while (inputStream.read(buffer).also { len1 = it } != -1) {
+                    fos.write(buffer, 0, len1)
+                    downloaded += len1
+                    per = (downloaded * 100 / totalSize)
+                    publishProgress(per.toInt())
+                }
+                fos.close()
+                inputStream.close()
+                openNewVersion(outputFile.path)
+                flag = true
+            } catch (e: MalformedURLException) {
+                Log.e("DownloadApk", "Update Error: " + e.message)
+                flag = false
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            return flag
+        }
+
+        private fun openNewVersion(location: String) {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setDataAndType(
+                getUriFromFile(location),
+                "application/vnd.android.package-archive"
+            )
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            context.startActivity(intent)
+        }
+
+        private fun getUriFromFile(filePath: String): Uri {
+            return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+                Uri.fromFile(File(filePath))
+            } else {
+                FileProvider.getUriForFile(
+                    context,
+                    context.packageName + ".provider",
+                    File(filePath)
+                )
+            }
+        }
+    }
+}
